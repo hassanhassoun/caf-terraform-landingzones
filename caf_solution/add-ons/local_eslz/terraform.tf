e956ea7c9d3d9d22752c4c465793625f831daf2a
Hassan Hassoun <hassan.hassoun@avanade.com>
2021-12-29 21:32:33 +0000

    eslz v1.1.1

---
+++ caf_solution/add-ons/local_eslz/terraform.tf
@@ -0,0 +1,18 @@
+# Configure Terraform to set the required AzureRM provider
+# version and features{} block.
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.77.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+provider "azurerm" {
+  partner_id = "ca4078f8-9bc4-471b-ab5b-3af6b86a42c8"
+  # partner identifier for CAF Terraform landing zones.
+  features {}
+}
