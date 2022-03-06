5000cd722b576f21e3e4b2c3acff10db4acb42c9
Hassan Hassoun <mail@hassanhassoun.com>
2022-03-06 00:27:18 +0000

    Initial vnet data

--- caf_solution/remote_data.tf
+++ caf_solution/remote_data.tf
@@ -3,3 +3,15 @@ data "azurerm_storage_account" "remote_data" {
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
 }
+
+module "data_virtual_networks" {
+  for_each = try(var.remote_data.virtual_networks, {})
+  source  = "./data/virtual_networks"
+  virtual_network_name = each.value.name 
+  resource_group_name = each.value.resource_group_name
+}
+
+
+output debug {
+  value = module.data_virtual_networks.*
+}
